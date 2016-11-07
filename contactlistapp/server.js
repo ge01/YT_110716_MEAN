89f5a5568ba38e4830e29c8946765ff807adbf9d
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-07 11:19:36 -0600

    two way binding with angularjs

--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+
+/*
 app.get('/', function (req, res){
   res.send("Hello World from server.js");
 });
+*/
 
 app.listen(3000);
 console.log("Server running on port 3000");
