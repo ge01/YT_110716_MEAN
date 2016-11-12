60cfb15427944b635a4b6537e3ca6d318a2c8050
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-12 09:32:33 -0600

    04_111216

--- 111016_MEAN/contactlistapp/server.js
+++ 111016_MEAN/contactlistapp/server.js
@@ -1,30 +1,27 @@
 // Connect node to express
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.get('/contactlist', function(req, res){
+app.get('/contactlist', function (req, res) {
   console.log("I received a GET request");
 
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '(111) 111-1111'
-  };
-  person2 = {
-    name: 'Emily',
-    email: 'emily@email.com',
-    number: '(222) 222-2222'
-  };
-  person3 = {
-    name: 'John',
-    email: 'john@email.com',
-    number: '(333) 333-3333'
-  };
+  db.contactlist.find(function (err,docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
 
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist);
+app.post('/contactlist', function(req,res){
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc){
+    res.json(doc);
+  });
 });
 
 app.listen(3000);
