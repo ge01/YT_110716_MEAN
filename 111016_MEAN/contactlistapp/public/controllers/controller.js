69f444c0bee99121c96aa0fc15f2ea0b9d54f0cc
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-12 11:42:34 -0600

    05_111216

--- 111016_MEAN/contactlistapp/public/controllers/controller.js
+++ 111016_MEAN/contactlistapp/public/controllers/controller.js
@@ -18,6 +18,31 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
       console.log(response);
       refresh();
     });
-
   };
+
+  $scope.remove = function(id){
+    console.log(id);
+    $http.delete('/contactlist/' + id).success(function(response){
+      refresh();
+    });
+  };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/contactlist/' + id).success(function(response) {
+      $scope.contact = response;
+    });
+  };
+
+  $scope.update = function(){
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+      refresh();
+    });
+  };
+
+$scope.deselect = function(){
+  $scope.contact = "";
+};
+
 }]);
