72a818cb8c7077ff3a79dfabe57c91f0c55e2f61
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-12-20 20:34:22 -0600

    01_122016

--- 121916_MEAN/contactlistapp/server.js
+++ 121916_MEAN/contactlistapp/server.js
@@ -1,2 +1,8 @@
 // Connect node to express
 var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000);
+console.log("Server running on port 3000");
