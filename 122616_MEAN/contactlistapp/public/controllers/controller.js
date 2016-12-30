cefcc52fbdab1ba045f74cb6e8962c9dfab3434e
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-12-29 20:56:10 -0600

    02_122916_000952

--- 122616_MEAN/contactlistapp/public/controllers/controller.js
+++ 122616_MEAN/contactlistapp/public/controllers/controller.js
@@ -2,22 +2,10 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello World from controller");
 
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '111-111-1111'
-  };
-  person2 = {
-    name: 'Emily',
-    email: 'emily@email.com',
-    number: '222-222-2222'
-  };
-  person3 = {
-    name: 'John',
-    email: 'john@email.com',
-    number: '333-333-3333'
-  };
+  $http.get('/contactlist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+  
+  });
 
-  var contactlist = [person1, person2, person3];
-  $scope.contactlist = contactlist;
 }]);
